c63cfda157105aafe20902a0eb3f9e0848d3d8b1
Mirami <mishaivanov99@gmail.com>
2020-08-26 15:55:47 +0500

    neutral

--- project/project/settings_example.py
+++ project/project/settings_example.py
@@ -7,7 +7,12 @@ DEBUG = ''
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
-    {
-        
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': u'',
+        'USER': u'',
+        'PASSWORD': u'',
+        'HOST': '',
+        'PORT': ,
+    },
 }
