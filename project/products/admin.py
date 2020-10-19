ea9a0352bb34ec4677ef5d341ec3d1175600d39d
Mirami <mishaivanov99@gmail.com>
2020-10-19 15:27:21 +0500

    Добавлены методы для прочтения json файлов и занесения в БД в task.py

--- project/products/admin.py
+++ project/products/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
+from django import forms
 
 # Register your models here.
+
+
+admin.site.register(Brands)
+admin.site.register(Category)
+admin.site.register(Collection)
+admin.site.register(GenderType)
+admin.site.register(HashTag)
+admin.site.register(Manufacturer)
+admin.site.register(TypeModel)
+admin.site.register(Size)
+admin.site.register(Image)
+admin.site.register(NomenSet)
+admin.site.register(Characteristics)
+admin.site.register(Product)
+admin.site.register(Set)
+
+
+class ColorAdmin(admin.ModelAdmin):
+    list_per_page = 100
+
+    search_fields = ['name', 'nameid', 'guid', 'nametwo', 'hexcode']
+    filter_horizontal = ('images',)
+
+
+admin.site.register(Color, ColorAdmin)
--- project/products/models.py
+++ project/products/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 class Brands(models.Model):
-    """
-    Добавить уровни (Структура дерева ?)
-    """
     name = models.CharField(verbose_name='Название бренда', max_length=256, unique=True)
     guid = models.CharField(verbose_name='guid', max_length=100)
 
@@ -13,10 +10,13 @@ class Brands(models.Model):
         verbose_name_plural = 'Бренды'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class Category(models.Model):
+    """
+    Добавить уровни (Структура дерева ?)
+    """
     name = models.CharField(verbose_name='Название категории', max_length=200)
     guid = models.CharField(verbose_name='guid', max_length=100)
     level = models.PositiveSmallIntegerField(verbose_name='Уровень вложенности', default=0)
@@ -26,7 +26,7 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class Collection(models.Model):
@@ -38,7 +38,7 @@ class Collection(models.Model):
         verbose_name_plural = 'Коллекции'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class Design(models.Model):
@@ -50,7 +50,7 @@ class Design(models.Model):
         verbose_name_plural = 'Дизайны'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class GenderType(models.Model):
@@ -62,7 +62,7 @@ class GenderType(models.Model):
         verbose_name_plural = 'Половая принадлежность'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class HashTag(models.Model):
@@ -74,7 +74,7 @@ class HashTag(models.Model):
         verbose_name_plural = 'Хэш-Теги'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class Manufacturer(models.Model):
@@ -86,19 +86,20 @@ class Manufacturer(models.Model):
         verbose_name_plural = 'Производители'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class TypeModel(models.Model):
     name = models.CharField(verbose_name='Название типа', max_length=200, unique=True)
     guid = models.CharField(verbose_name='guid', max_length=100)
+    nametypeid = models.PositiveIntegerField(verbose_name='id типа')
 
     class Meta:
         verbose_name = 'Тип'
         verbose_name_plural = 'Типы'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class Size(models.Model):
@@ -110,18 +111,18 @@ class Size(models.Model):
         verbose_name_plural = 'Размеры'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
 
 class Image(models.Model):
     """
     Типы и подтипы картинок?
     """
-    imagename = models.CharField(verbose_name='Название картинки', max_length=200)
-    guid = models.CharField(verbose_name='guid', max_length=100)
+    imagename = models.TextField(verbose_name='Название картинки')
+    guid = models.TextField(verbose_name='guid')
     imagesize = models.PositiveIntegerField(verbose_name='Размер картинки')
-    imageext = models.CharField(verbose_name='Расширение картинки', max_length=20)
-    imagemask = models.ImageField(verbose_name='Путь к картинке', upload_to='images/%Y/%m/', blank=True)
+    imageext = models.TextField(verbose_name='Расширение картинки')
+    imagemask = models.ImageField(verbose_name='Путь к картинке', upload_to='images/%Y/%m/', blank=True, max_length=500)
     imagetype = models.PositiveSmallIntegerField(verbose_name='Тип картинки', default=0)
     imagesubtype = models.PositiveSmallIntegerField(verbose_name='Подтип картинки', null=True)
 
@@ -130,9 +131,11 @@ class Image(models.Model):
         verbose_name_plural = 'Изображения'
 
     def __str__(self):
-        return self.imagename
+        return self.imagemask.__str__()
+
+
 class NomenSet(models.Model):
-    namesetprodtype = models.SmallIntegerField(verbose_name='Положение в наборе',unique=True)
+    namesetprodtype = models.SmallIntegerField(verbose_name='Положение в наборе', unique=True)
     guid = models.CharField(verbose_name='guid', max_length=100)
 
     class Meta:
@@ -140,36 +143,55 @@ class NomenSet(models.Model):
         verbose_name_plural = 'Положения товаров в наборе'
 
     def __str__(self):
-        return self.name
+        return self.guid
 
-class ProductOffer(models.Model):
-    name = models.CharField(verbose_name='Торговое предложение', max_length=256)
+
+class Color(models.Model):
+    nameid = models.PositiveIntegerField(verbose_name='id имени', unique=True)
+    name = models.CharField(verbose_name='Название цвета', max_length=200)
     guid = models.CharField(verbose_name='guid', max_length=100)
-    article = models.CharField(verbose_name='Артикул', max_length=30)
-    description = models.TextField(verbose_name='Описание')
+    nametwo = models.CharField(verbose_name='Дополнительное название', max_length=200, null=True)
+    hexcode = models.CharField(verbose_name='Цвет в числовом представлении', max_length=100, null=True)
+    images = models.ManyToManyField(Image, verbose_name='Фотографии', related_name='color_images')
+
+    class Meta:
+        verbose_name = 'Цвет'
+        verbose_name_plural = 'Цвета'
+
+    def __str__(self):
+        return self.guid
+
+
+class Characteristics(models.Model):
+    name = models.CharField(verbose_name='Характеристики товара', max_length=256)
+    guid = models.CharField(verbose_name='guid', max_length=200)
+    article = models.TextField(verbose_name='Артикул', null=True)
+    description = models.TextField(verbose_name='Описание', null=True)
+    composition = models.TextField(verbose_name='Сравнение', null=True)
     smallsize = models.BooleanField(verbose_name='Маленький ли размер?')
     length = models.PositiveIntegerField(verbose_name='Длина')
     width = models.PositiveIntegerField(verbose_name='Ширина')
     height = models.PositiveIntegerField(verbose_name='Высота')
     weight = models.FloatField(verbose_name='Вес')
     isshoes = models.BooleanField(verbose_name='Обувь?')
-    # artexh
+    artexh = models.CharField(verbose_name='Доп инфа + id цвета', max_length=256)
     mainprice = models.FloatField(verbose_name='Цена', default=0.0, blank=True)
     mainold_price = models.FloatField(verbose_name='Старая цена', default=0.0, blank=True)
     is_new = models.BooleanField(verbose_name='Новинка?')
     is_hit = models.BooleanField(verbose_name='Хит продаж?')
-    is_mainphoto = models.ForeignKey(Image, verbose_name='Фотография', on_delete=models.CASCADE)
+    is_mainphoto = models.ForeignKey(Image, verbose_name='Фотография', on_delete=models.CASCADE, null=True)
     is_set = models.BooleanField(verbose_name='Набор?')
     is_sale = models.BooleanField(verbose_name='Распродажа?')
-    # maintenance
+    maintenance = models.TextField(verbose_name='Уход за товаром', null=True)
 
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-    collection = models.ForeignKey(Collection, verbose_name='Коллекция', on_delete=models.CASCADE)
-    brand = models.ForeignKey(Brands, on_delete=models.CASCADE, verbose_name='Бренд')
-    gender_type = models.ForeignKey(GenderType, verbose_name='Половая принадлежность', on_delete=models.CASCADE)
-    manufacturer = models.ForeignKey(Manufacturer, verbose_name='Производитель', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE, null=True)
+    collection = models.ForeignKey(Collection, verbose_name='Коллекция', on_delete=models.CASCADE, null=True)
+    brand = models.ForeignKey(Brands, on_delete=models.CASCADE, verbose_name='Бренд', null=True)
+    gender_type = models.ForeignKey(GenderType, verbose_name='Половая принадлежность', on_delete=models.CASCADE,
+                                    null=True)
+    manufacturer = models.ForeignKey(Manufacturer, verbose_name='Производитель', on_delete=models.CASCADE, null=True)
 
-    prodtypes = models.ForeignKey(TypeModel, verbose_name='Тип', on_delete=models.CASCADE)
+    prodtypes = models.ManyToManyField(TypeModel, verbose_name='Тип')
     designs = models.ManyToManyField(Design, verbose_name='Дизайн')
     images = models.ManyToManyField(Image, verbose_name='Фотографии', related_name='image_set')
     analogs = models.ManyToManyField("self", verbose_name='Аналоги')
@@ -177,71 +199,74 @@ class ProductOffer(models.Model):
     count = models.PositiveSmallIntegerField(verbose_name='Количество', default=0)
 
     class Meta:
-        verbose_name = 'Предложение'
-        verbose_name_plural = 'Предложения'
+        verbose_name = 'Характеристики'
+        verbose_name_plural = 'Характеристики'
 
     def __str__(self):
-        return self.name
+        return self.guid
+
+
+class Set(models.Model):
+    name = models.CharField(verbose_name='Торговое предложение', max_length=256)
+    guid = models.CharField(verbose_name='guid', max_length=100)
+    article = models.CharField(verbose_name='Артикул', max_length=30)
+    description = models.TextField(verbose_name='Описание', null=True)
+    composition = models.TextField(verbose_name='Сравнение', null=True)
+    smallsize = models.BooleanField(verbose_name='Маленький ли размер?')
+    maintenance = models.TextField(verbose_name='Уход за товаром', null=True)
+    length = models.PositiveIntegerField(verbose_name='Длина')
+    width = models.PositiveIntegerField(verbose_name='Ширина')
+    height = models.PositiveIntegerField(verbose_name='Высота')
+    weight = models.FloatField(verbose_name='Вес')
+    isshoes = models.BooleanField(verbose_name='Обувь?', null=True)
+    mainprice = models.FloatField(verbose_name='Цена', default=0.0, blank=True)
+    subprodtype = models.PositiveSmallIntegerField(verbose_name='Подтип', default=0)
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE, null=True)
+    collection = models.ForeignKey(Collection, verbose_name='Коллекция', on_delete=models.CASCADE, null=True)
+    brand = models.ForeignKey(Brands, on_delete=models.CASCADE, verbose_name='Бренд', null=True)
+    manufacturer = models.ForeignKey(Manufacturer, verbose_name='Производитель', on_delete=models.CASCADE, null=True)
+
+    is_new = models.BooleanField(verbose_name='Новинка?')
+    is_hit = models.BooleanField(verbose_name='Хит продаж?')
+    is_mainphoto = models.ForeignKey(Image, verbose_name='Фотография', on_delete=models.CASCADE, null=True)
+    is_set = models.BooleanField(verbose_name='Набор?')
+    is_sale = models.BooleanField(verbose_name='Распродажа?')
+
+    # maintenance
+    gender_type = models.ForeignKey(GenderType, verbose_name='Половая принадлежность', on_delete=models.CASCADE,
+                                    null=True)
+
+    images = models.ManyToManyField(Image, verbose_name='Фотографии', related_name='images_in_productset')
+    analogs = models.ManyToManyField("self", verbose_name='Аналоги')
+    addprods = models.ManyToManyField("self", verbose_name='Похожие')
+
+    nomen_sets = models.ManyToManyField(NomenSet, verbose_name='Положение товаров в наборе')
+
+    class Meta:
+        verbose_name = 'Набор'
+        verbose_name_plural = 'Наборы'
+
+    def __str__(self):
+        return self.guid
 
 
 class Product(models.Model):
     name = models.CharField(verbose_name='Название продукта', max_length=200)
     guid = models.CharField(verbose_name='guid', max_length=100)
-    # isshoestatamatr
     isshoes = models.BooleanField(verbose_name='Обувь?')
-    articleh = models.CharField(verbose_name='Артикул', max_length=40)
+    articleh = models.CharField(verbose_name='Артикул', max_length=40, null=True)
     price = models.FloatField(verbose_name='Цена', default=0.0, blank=True)
     old_price = models.FloatField(verbose_name='Старая цена', default=0.0, blank=True)
     count = models.PositiveSmallIntegerField(verbose_name='Количество', default=0)
-    product_offer = models.ForeignKey(ProductOffer, on_delete=models.SET_NULL, null=True, related_name='product',
-                                      verbose_name='Торговое предложение')
+    characteristics = models.ForeignKey(Characteristics, on_delete=models.CASCADE, null=True, related_name='product',
+                                        verbose_name='Характеристики товара')
     images = models.ManyToManyField(Image, verbose_name='Фотографии', related_name='images')
-    size = models.ForeignKey(Size, verbose_name='Размер', on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, verbose_name='Размер', on_delete=models.CASCADE, null=True)
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
     def __str__(self):
-        return self.name
-
-    class ProductOfferSet(models.Model):
-        name = models.CharField(verbose_name='Торговое предложение', max_length=256)
-        guid = models.CharField(verbose_name='guid', max_length=100)
-        article = models.CharField(verbose_name='Артикул', max_length=30)
-        description = models.TextField(verbose_name='Описание')
-        smallsize = models.BooleanField(verbose_name='Маленький ли размер?')
-        length = models.PositiveIntegerField(verbose_name='Длина')
-        width = models.PositiveIntegerField(verbose_name='Ширина')
-        height = models.PositiveIntegerField(verbose_name='Высота')
-        weight = models.FloatField(verbose_name='Вес')
-        isshoes = models.BooleanField(verbose_name='Обувь?')
-        mainprice = models.FloatField(verbose_name='Цена', default=0.0, blank=True)
-        subprodtype = models.PositiveSmallIntegerField(verbose_name='Подтип', default=0)
-
-        category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-        collection = models.ForeignKey(Collection, verbose_name='Коллекция', on_delete=models.CASCADE)
-        brand = models.ForeignKey(Brands, on_delete=models.CASCADE, verbose_name='Бренд')
-        manufacturer = models.ForeignKey(Manufacturer, verbose_name='Производитель', on_delete=models.CASCADE)
-
-        is_new = models.BooleanField(verbose_name='Новинка?')
-        is_hit = models.BooleanField(verbose_name='Хит продаж?')
-        is_mainphoto = models.ForeignKey(Image, verbose_name='Фотография', on_delete=models.CASCADE)
-        is_set = models.BooleanField(verbose_name='Набор?')
-        is_sale = models.BooleanField(verbose_name='Распродажа?')
-
-        # maintenance
-        gender_type = models.ForeignKey(GenderType, verbose_name='Половая принадлежность', on_delete=models.CASCADE)
-
-        images = models.ManyToManyField(Image, verbose_name='Фотографии', related_name='images_in_productset')
-        analogs = models.ManyToManyField("self", verbose_name='Аналоги')
-        addprods = models.ManyToManyField("self", verbose_name='Похожие')
-
-        nomen_sets= models.ManyToManyField(NomenSet,verbose_name='Положение товаров в наборе')
-
-        class Meta:
-            verbose_name = 'Набор'
-            verbose_name_plural = 'Наборы'
-
-        def __str__(self):
-            return self.name
+        return self.guid
